6979f860e09068eb2102775c3ad8bc6f1eb03589
islam nassar <52415654+islamnasar@users.noreply.github.com>
2026-01-13 21:38:30 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 app.get("/", (req, res) => {
   res.send("BNGC server is running ✅");
 });
+app.get("/ip", async (req, res) => {
+  const r = await fetch("https://api.ipify.org?format=json");
+  res.json(await r.json());
+});
